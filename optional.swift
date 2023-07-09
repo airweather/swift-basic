5faaf29b3bc42cd03774e46b2b2da0c505696fc6
Jun <wnsghd1@naver.com>
2023-07-09 23:51:38 +0900

    Optional binding

--- optional.swift
+++ optional.swift
@@ -50,3 +50,59 @@ if let value = optionalValue {
 } else {
     print("nil")
 }
+
+// 옵셔널 강제 추출
+
+// 옵셔널에 값이 들어있는지 확인하지 않고 강제로 값을 꺼내는 방식
+// 만약 값이 없을경우(nil) 런타임 오류가 발생하여 프로그램이 강제 종료됨
+
+var optionalValue: Int? = 100
+print(optionalValue)
+
+var optionalValue: Int? = nil
+print(optionalValue)
+
+var optionalValue: Int! = 100
+print(optionalValue)
+
+var optionalValue: Int! = nil
+print(optionalValue)
+
+// 옵셔널 바인딩
+
+// 옵셔널에 값이 들어있는지 확인하고 값이 있으면 값을 꺼내옴과 동시에 if-let 구문 내부에서 사용 가능
+
+var optionalValue: Int? = 100
+
+if let value = optionalValue {
+    print(value)
+} else {
+    print("nil")
+}
+
+var optionalValue: Int? = nil
+
+if let value = optionalValue {
+    print(value)
+} else {
+    print("nil")
+}
+
+var optionalValue: Int! = 100
+
+if let value = optionalValue {
+    print(value)
+} else {
+    print("nil")
+}
+
+var optionalValue: Int! = nil
+
+if let value = optionalValue {
+    print(value)
+} else {
+    print("nil")
+}
+
+// 옵셔널 강제 추출은 옵셔널에 값이 들어있는지 확인하지 않고 강제로 값을 꺼내는 방식
+// 만약 값이 없을경우(nil) 런타임 오류가 발생하여 프로그램이 강제 종료됨
