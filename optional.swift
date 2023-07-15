d691cc8e2173c0ccced47b1b063ed8394534836d
Jun <wnsghd1@naver.com>
2023-07-15 23:49:06 +0900

    Optional Chaining

--- optional.swift
+++ optional.swift
@@ -118,3 +118,40 @@ print(optionalValue)
 
 // 옵셔널 체이닝
 // 옵셔널의 내부의 내부의 내부로 옵셔널이 연결되어 있을 때 유용하게 사용할 수 있음
+
+class Person {
+    var name: String?
+    var job: String?
+    var home: Apartment?
+}
+
+
+class Apartment {
+    var buildingNumber: String?
+    var roomNumber: String?
+    var `guard`: Person?
+}
+
+
+let yagom: Person? = Person()
+let apart: Apartment? = Apartment()
+let superman: Person? = Person()
+
+// 옵셔널 체이닝이 실행 후 결과값이 nil일 수 있으므로 결과 타입도 옵셔널입니다
+// 만약 우리집의 경비원의 직업이 궁금하다면..?
+
+// 옵셔널 체이닝을 사용하지 않는다면...
+func guardJob(owner: Person?) {
+    if let owner = owner {
+        if let home = owner.home {
+            if let `guard` = home.guard {
+                if let guardJob = `guard`.job {
+                    print("우리집 경비원의 직업은 \(guardJob)입니다")
+                } else {
+                    print("우리집 경비원은 직업이 없어요")
+                }
+            }
+        }
+    }
+}
+
